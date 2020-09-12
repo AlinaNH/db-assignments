4dd6617c71866eb4dc33de1e2dfee2860b3a46a5
Alina <nazarovich.alina@gmail.com>
2020-09-12 16:07:43 +0300

    add more optimization

--- task/mongo-optimization-task.js
+++ task/mongo-optimization-task.js
@@ -13,7 +13,7 @@ const ObjectId = require('mongodb').ObjectID;
  * */
 async function before(db) {
     await db.collection('opportunities').createIndex({'initiativeId': 1, "contacts.questions.category_id": 1});
-    await db.collection('clientCriteria').createIndex({'value': 1});
+    await db.collection('clientCriteria').createIndex({'value': 1, 'versions.initiativeId': 1});
 }
 
 /**
@@ -232,6 +232,9 @@ async function task_3_1(db) {
                             "versions.initiativeId" : ObjectId("58af4da0b310d92314627290"),
                             $expr: { $eq: [ "$value",  "$$item" ] }
                         }
+                    },
+                    {
+                        "$limit": 1
                     }
                 ],
                 "as" : "criteria"
